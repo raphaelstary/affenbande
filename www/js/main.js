918caf5ad284fab323dd52b0ac99385477f31f8a
raphaelstary <raphael.stary@gmail.com>
2015-07-16 15:42:44 +0200

    custom game analytics events

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,9 @@
 window.onload = function () {
     "use strict";
 
+    window.appName = 'affenbande';
+    window.appVersion = '0.1.0';
     var app = Bootstrapper.atlas().pointer().responsive().orientation().visibility().analytics('https://localhost:8088',
-        'HA-aec4a816', 'affenbande').fullScreen().build(MyGameResources);
+        'HA-aec4a816', window.appName).fullScreen().build(MyGameResources);
     app.start();
 };
