d826d425450f373aaa750c58920e9f93bcb3325f
raphaelstary <raphael.stary@gmail.com>
2015-07-22 13:14:36 +0200

    setup analytics before everything else

--- www/js/main.js
+++ www/js/main.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
     window.appName = 'affenbande';
     window.appVersion = '0.1.0';
-    var app = Bootstrapper.atlas().pointer().responsive().orientation().visibility().analytics('https://localhost:8088',
-        'HA-aec4a816', window.appName).fullScreen().build(MyGameResources);
+    var app = Bootstrapper.analytics('https://localhost:8088', 'HA-aec4a816',
+        window.appName).atlas().pointer().responsive().orientation().visibility().fullScreen().build(MyGameResources);
     app.start();
 };
