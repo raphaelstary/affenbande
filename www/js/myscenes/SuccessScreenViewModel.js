a9b5d1dad305d55fa69b3de362bbfef4fe7a4109
raphaelstary <raphael.stary@gmail.com>
2015-10-10 18:47:45 +0200

    zindex layering and small stuff

--- www/js/myscenes/SuccessScreenViewModel.js
+++ www/js/myscenes/SuccessScreenViewModel.js
@@ -2,9 +2,17 @@ var SuccessScreenViewModel = (function () {
     "use strict";
 
     function SuccessScreenViewModel(services) {
-        this.timer = services.timer;
-        this.sceneStorage = services.sceneStorage;
+        this.stage = services.stage;
     }
+
+    SuccessScreenViewModel.prototype.postConstruct = function () {
+        this.bg = this.stage.createRectangle(true).setWidth(Width.FULL).setHeight(Height.FULL).setPosition(Width.HALF,
+            Height.HALF).setColor('black').setAlpha(0.5).setZIndex(6);
+    };
+
+    SuccessScreenViewModel.prototype.preDestroy = function () {
+        this.bg.remove();
+    };
 
     SuccessScreenViewModel.prototype.doSkip = function () {
         this.nextScene();
